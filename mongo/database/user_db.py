20b9622ea272148c766fabde3bb0467eb0f86967
cjhcw <820397819@qq.com>
2018-09-17 20:30:39 +0800

    token

--- mongo/database/user_db.py
+++ mongo/database/user_db.py
@@ -2,7 +2,7 @@
 """
 __author__:cjhcw
 """
-
+import jwt
 from datetime import datetime
 from core_manager.mongo_manager import mongo_manager
 
@@ -24,6 +24,11 @@ def login(data):
     if user and user['pwd'] == data['pwd']:
         mongo_manager.update_one(users_collection, {'account': data['account']},
                                  {"$set": {'login_time': datetime.utcnow()}})
-        return True
+        encoded = jwt.encode(
+            {'account': user.account, '_id': str(user._id), 'organization': str(user.level),
+             'update_time': str(datetime.utcnow())}, 'secret', algorithm='HS256')
+        return_object = {'message': 'success', 'token': encoded,
+                         'account': user.account, 'org': str(user.level), '_id': str(user._id)}
+        return return_object
     else:
         return False
