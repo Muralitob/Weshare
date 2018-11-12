fa7064c0aaf1d4e7f84104c13951dab44f961d41
cjhcw <820397819@qq.com>
2018-11-12 20:37:46 +0800

    二手市场

--- mongo/route/goods.py
+++ mongo/route/goods.py
@@ -7,11 +7,12 @@ from flask import Blueprint, request, jsonify
 from database.users_db import requires_auth
 from database import goods_db
 import jwt
+import utility
 
-goods = Blueprint("goods", __name__, url_prefix='/api/news')
+goods = Blueprint("goods", __name__, url_prefix='/api/goods')
 
 
-@goods.route('/send_goods', methods=["POST", "DELETE", "PUT"])
+@goods.route('/send_goods', methods=['POST', 'DELETE', 'PUT'])
 @requires_auth
 def send_goods():
     """
@@ -44,3 +45,20 @@ def send_goods():
             return jsonify({"message": "编辑商品成功", "code": 405}), 200
         else:
             return jsonify({"message": "编辑商品失败", "code": 406}), 404
+
+
+@goods.route('/get_goods', methods=["GET"])
+def get_goods():
+    """
+
+    :return:
+    """
+    page = request.args.get("page")
+    limit = request.args.get("limit")
+    uid = request.args.get("uid")
+    if not uid:
+        token = request.headers.get('Authorization')
+        token = jwt.decode(token[6:], 'secret', algorithms=['HS256'])
+        uid = token['uid']
+    goods, length = goods_db.get_goods(int(uid), page, int(limit))
+    return jsonify({"news": utility.convert_to_json(goods), "total": length}), 200
